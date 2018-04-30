3ca1891a8e28ea744e07825c971523223435e012
wen-dell <wendellmccaulay@hotmail.com>
2018-04-29 23:35:12 -0300

    Usei a anotação @Before

--- src/br/com/casadocodigo/testes/CalculadoraTest.java
+++ src/br/com/casadocodigo/testes/CalculadoraTest.java
@@ -2,27 +2,32 @@ package br.com.casadocodigo.testes;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.casadocodigo.classes.Calculadora;
 
 public class CalculadoraTest {
 	
+	private Calculadora calculadora;
+	
+	@Before
+	public void inicializa() {
+		this.calculadora = new Calculadora();
+	}
+	
 	@Test
 	public void deveSomar1Com1() {
-		Calculadora calculadora = new Calculadora();
 		assertEquals(2, calculadora.soma(1, 1));
 	}
 	
 	@Test
 	public void deveSomarVariosValores() {
-		Calculadora calculadora = new Calculadora();
 		assertEquals(15, calculadora.soma(1, 2, 3, 4, 5));
 	}
 	
 	@Test
 	public void deveCalcularFatorial5() {
-		Calculadora calculadora = new Calculadora();
 		assertEquals(120, calculadora.fatorial(5));
 	}
 
