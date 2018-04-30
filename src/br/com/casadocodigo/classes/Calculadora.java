482554687a0ec797c6d8a45657440527946d0b61
wen-dell <wendellmccaulay@hotmail.com>
2018-04-29 23:32:17 -0300

    Adicionei mais dois métodos de teste

--- src/br/com/casadocodigo/classes/Calculadora.java
+++ src/br/com/casadocodigo/classes/Calculadora.java
@@ -6,4 +6,26 @@ public class Calculadora {
 		return i + j;
 	}
 
+	public int soma(int... numeros) {
+		int soma = 0;
+		
+		for (int numero : numeros) {
+			soma += numero;
+		}
+		
+		return soma;
+	}
+
+	public int fatorial(int i) {
+		int fatorial = 1;
+		
+		int n = 1;
+		while (n <= i) {
+			fatorial *= n;
+			n += 1;
+		}
+		
+		return fatorial;
+	}
+
 }
--- src/br/com/casadocodigo/testes/CalculadoraTest.java
+++ src/br/com/casadocodigo/testes/CalculadoraTest.java
@@ -13,5 +13,17 @@ public class CalculadoraTest {
 		Calculadora calculadora = new Calculadora();
 		assertEquals(2, calculadora.soma(1, 1));
 	}
+	
+	@Test
+	public void deveSomarVariosValores() {
+		Calculadora calculadora = new Calculadora();
+		assertEquals(15, calculadora.soma(1, 2, 3, 4, 5));
+	}
+	
+	@Test
+	public void deveCalcularFatorial5() {
+		Calculadora calculadora = new Calculadora();
+		assertEquals(120, calculadora.fatorial(5));
+	}
 
 }
