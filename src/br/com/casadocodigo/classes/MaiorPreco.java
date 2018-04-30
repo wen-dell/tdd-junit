0808635baa0cd418985617383f8e827863f32444
wen-dell <wendellmccaulay@hotmail.com>
2018-04-30 18:30:44 -0300

    Nova implementação do método encontra()

--- src/br/com/casadocodigo/classes/MaiorPreco.java
+++ src/br/com/casadocodigo/classes/MaiorPreco.java
@@ -4,7 +4,15 @@ public class MaiorPreco {
 
 	public double encontra(CarrinhoDeCompras carrinho) {
 		if (carrinho.getItens().size() == 0) return 0;
-		return carrinho.getItens().get(0).getValorTotal();
+		
+		double maior = carrinho.getItens().get(0).getValorTotal();
+		for (Item item : carrinho.getItens()) {
+			if (maior < item.getValorTotal()) {
+				maior = item.getValorTotal();
+			}
+		}
+		
+		return maior;
 	}
 
 }
--- src/br/com/casadocodigo/testes/MaiorPrecoTest.java
+++ src/br/com/casadocodigo/testes/MaiorPrecoTest.java
@@ -30,5 +30,18 @@ public class MaiorPrecoTest {
 		
 		assertEquals(900.0, valor, 0.0001);
 	}
+	
+	@Test
+	public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
+		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+		carrinho.adiciona(new Item("Fogão",1 , 1500.0));
+		carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
+		
+		MaiorPreco algoritmo = new MaiorPreco();
+		double valor = algoritmo.encontra(carrinho);
+		
+		assertEquals(1500.0, valor, 0.0001);
+	}
 
 }
